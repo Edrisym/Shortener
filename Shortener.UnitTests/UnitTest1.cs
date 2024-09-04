a288267104d73e52df61d5a0254e6e6eae007b66
Eddie <edrismoavenghafouri@gmail.com>
2024-09-04 20:58:49 +0330

    Project first commit

---
+++ Shortener.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Shortener.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
