85b3066840794037c70113464747efff29f8e5d7
Eddie <edrismoavenghafouri@gmail.com>
2024-09-11 14:07:23 +0330

    Moving renaming files

---
+++ Shortener/Common/Models/AppSettings.cs
@@ -0,0 +1,8 @@
+namespace Shortener.Common.Models;
+
+public class AppSettings
+{
+    public int HashParts { get; init; }
+    public string ConnectionString { get; set; } = default!;
+    public string DatabaseName { get; set; } = default!;
+}
