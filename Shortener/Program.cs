18f22cc179170e1b2661ee17f2b18983cb358644
Eddie <edrismoavenghafouri@gmail.com>
2024-09-17 22:15:53 +0330

    .

--- Shortener/Program.cs
+++ Shortener/Program.cs
@@ -1,33 +1,25 @@
-// Put Program here because tests wouldn't recognise the class  
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.ConfigureAppSettings();
+builder.AddRateLimiting();
+builder.ConfigureDbContext();
+builder.Services.AddServices();
+builder.Services.AddFluentApiValidation();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.ConfigureAppSettings();
-        builder.AddRateLimiting();
-        builder.ConfigureDbContext();
-        builder.Services.AddServices();
-        builder.Services.AddFluentApiValidation();
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-
-        var app = builder.Build();
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.MapShortenEndpoint();
-        app.UseHttpsRedirection();
-
-        app.UseRateLimiter();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseForwardedHeaders();
+app.MapShortenEndpoint();
+app.UseHttpsRedirection();
+
+app.UseRateLimiter();
+
+app.Run();
