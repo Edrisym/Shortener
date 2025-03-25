3cafdc3729a20f3c7fc635605614b18f1addc4e6
Eddie <edrismoavenghafouri@gmail.com>
2025-03-25 17:03:51 +0330

    app settings

--- Shortener/Common/Models/AppSettings.cs
+++ Shortener/Common/Models/AppSettings.cs
@@ -3,7 +3,7 @@ namespace Shortener.Common.Models;
 public class AppSettings
 {
     public int HashParts { get; init; }
-    public string ConnectionString { get; set; } = default!;
-    public string DatabaseName { get; set; } = default!;
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
     public string BaseUrl { get; set; }
 }
