4aa3535dd0d4757dfecfd608d580c80666916ed1
Eddie <edrismoavenghafouri@gmail.com>
2025-03-25 16:11:17 +0330

    adding user table and added todo

---
+++ Shortener/Common/Models/User.cs
@@ -0,0 +1,12 @@
+namespace Shortener.Common.Models;
+
+
+//TODO
+public class User
+{
+    public string user_id { get; set; }
+    public string username { get; set; }
+    public string email { get; set; }
+    public string password_hash { get; set; }
+    public string created_at { get; set; }
+}
