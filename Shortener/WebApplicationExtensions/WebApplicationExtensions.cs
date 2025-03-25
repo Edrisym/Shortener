1b9d7c51ae9a2ee28b7439257d81db66f7e40cc7
Eddie <edrismoavenghafouri@gmail.com>
2025-03-25 17:03:27 +0330

    modified WebApplicationExtensions

--- Shortener/WebApplicationExtensions/WebApplicationExtensions.cs
+++ Shortener/WebApplicationExtensions/WebApplicationExtensions.cs
@@ -5,44 +5,33 @@ namespace Shortener.WebApplicationExtensions;
 
 public static class WebApplicationExtensions
 {
-    public static void ConfigureAppSettings(this WebApplicationBuilder builder)
-    {
-        builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
-    }
 
-    public static void AddServices(this IServiceCollection serviceCollection)
-    {
-        serviceCollection.AddScoped<IShortenService, ShortenService>();
-    }
 
-    public static void ConfigureDbContext(this WebApplicationBuilder builder)
+    public static void ConfigureDbContext(this WebApplicationBuilder builder , AppSettings settings)
     {
-        var settings = builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
         builder.Services.AddDbContext<ShortenerDbContext>(options =>
         {
-            if (settings is null)
-                throw new ArgumentNullException(nameof(settings));
             options.UseMongoDB(settings.ConnectionString, settings.DatabaseName);
         });
     }
 
-    public static void AddRateLimiting(this WebApplicationBuilder builder)
-    {
-        var myOptions = builder.Configuration.GetSection("MyRateLimitOptions").Get<MyRateLimitOptions>();
-
-        var slidingPolicy = "sliding";
-
-        builder.Services.AddRateLimiter(rateLimiterOptions =>
-        {
-            rateLimiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
-            rateLimiterOptions.AddSlidingWindowLimiter(policyName: slidingPolicy, options =>
-            {
-                options.PermitLimit = myOptions!.PermitLimit;
-                options.Window = TimeSpan.FromSeconds(myOptions.Window);
-                options.SegmentsPerWindow = myOptions.SegmentsPerWindow;
-                // options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-                // options.QueueLimit = myOptions.QueueLimit;
-            });
-        });
-    }
+    // public static void AddRateLimiting(this WebApplicationBuilder builder)
+    // {
+    //     var myOptions = builder.Configuration.GetSection("MyRateLimitOptions").Get<MyRateLimitOptions>();
+    //
+    //     var slidingPolicy = "sliding";
+    //
+    //     builder.Services.AddRateLimiter(rateLimiterOptions =>
+    //     {
+    //         rateLimiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    //         rateLimiterOptions.AddSlidingWindowLimiter(policyName: slidingPolicy, options =>
+    //         {
+    //             options.PermitLimit = myOptions!.PermitLimit;
+    //             options.Window = TimeSpan.FromSeconds(myOptions.Window);
+    //             options.SegmentsPerWindow = myOptions.SegmentsPerWindow;
+    //             // options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    //             // options.QueueLimit = myOptions.QueueLimit;
+    //         });
+    //     });
+    // }
 }
