ff81c29803ff6d361d5acec5ca27e87792c468bb
Eddie <edrismoavenghafouri@gmail.com>
2025-03-26 12:45:23 +0330

    removing api result pattern

--- Shortener/Common/BaseError.cs
+++
@@ -1,9 +0,0 @@
-using Shortener.Primitives.Response;
-
-namespace Shortener.Common;
-
-public abstract class BaseErrors
-{
-    public static Error InternalServerError => new("ApplicationFailure", "An Internal Server Error Has Occured");
-    public static Error IdentityConflictError => new("IdentityConflict", "Identity values do not match eachother");
-}
