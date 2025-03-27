7b9eb688736a2e6f0d3b33f4a7834f5da65290e1
Eddie <edrismoavenghafouri@gmail.com>
2025-03-28 01:14:08 +0330

    Refactored endpoints

--- Shortener/Endpoints/ShortenerEndpoints.cs
+++ Shortener/Endpoints/ShortenerEndpoints.cs
@@ -9,25 +9,29 @@ public static class ShortenerEndpoints
         app.MapGet("", GetUrls);
     }
 
-    private static async Task<List<UrlRecord>> GetUrls
-    (ShortenerDbContext dbContext,
+    private static async Task<List<UrlResponse>> GetUrls(
+        [FromServices] ShortenerDbContext dbContext,
         CancellationToken cancellationToken)
     {
         return await dbContext.Urls.Select(x =>
-                new UrlRecord(x.LongUrl, x.ShortCode, x.CreatedAt, x.ExpiresAt))
+                new UrlResponse(x.LongUrl, x.ShortCode, x.CreatedAt, x.ExpiresAt))
             .ToListAsync(cancellationToken);
     }
 
-    private static async Task<IResult> RedirectToUrl
-    ([FromQuery(Name = "code")] string code,
+    private static async Task<IResult> RedirectToUrl(
+        [FromQuery(Name = "code")] string code,
         ShortenerDbContext dbContext,
         CancellationToken cancellationToken)
     {
+        var request = new RedirectRequest
+        {
+            Code = code
+        };
         var shortenedUrl = await dbContext.Urls
-            .SingleOrDefaultAsync(s => s.ShortCode == code, cancellationToken);
+            .SingleOrDefaultAsync(s => s.ShortCode == request.Code, cancellationToken);
 
         if (shortenedUrl is null)
-            return Results.NotFound();
+            return Results.NotFound("No Url is found");
 
         if (DateTime.UtcNow > shortenedUrl.ExpiresAt)
             return Results.Problem(
@@ -35,31 +39,24 @@ public static class ShortenerEndpoints
                 statusCode: StatusCodes.Status422UnprocessableEntity);
 
 #pragma warning disable SYSLIB0013
-        // todo : find alternative 
+        // todo : find an alternative 
         var url = Uri.EscapeUriString(shortenedUrl.LongUrl);
 #pragma warning restore SYSLIB0013
 
         return Results.Redirect(url);
     }
 
-    private static async Task<IResult> ShortenUrl
-    ([FromQuery(Name = "longUrl")] string url,
-        IShortenService service,
+    private static async Task<IResult> ShortenUrl(
+        [FromQuery(Name = "longUrl")] string longUrl,
+        [FromServices] IShortenService service,
         CancellationToken cancellationToken)
     {
-        if (!url.IsUrlValid())
+        var request = new ShortenUrlRequest
         {
-            return Results.BadRequest(
-                "The provided longUrl is not valid. Please ensure it is a properly formatted URL.");
-        }
+            LongUrl = longUrl
+        };
 
-        var result = await service.ToShortUrl(url, cancellationToken);
+        var result = await service.ToShortUrl(request, cancellationToken);
         return Results.Json(new { LongUrl = result });
     }
-
-    private record UrlRecord(
-        string LongUrl,
-        string ShortCode,
-        DateTime CreatedAt,
-        DateTime ExpiresAt);
 }
