f6bbff876ffc2babfbf88af291fd31fab6bbb357
Eddie <edrismoavenghafouri@gmail.com>
2025-03-28 01:14:23 +0330

    removing namespaces

--- SharpYarp/Program.cs
+++ SharpYarp/Program.cs
@@ -1,9 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Yarp.ReverseProxy;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
