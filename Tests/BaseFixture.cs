d3b6b29c787976cfa0ae98ecaf11d838d4415e0d
Eddie <edrismoavenghafouri@gmail.com>
2025-04-07 21:10:14 +0330

    moving base fixture

---
+++ Tests/BaseFixture.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Shortener.Persistence;
+using Testcontainers.MongoDb;
+
+namespace Tests;
+
+public class BaseFixture : IAsyncLifetime
+{
+    private MongoDbContainer _mongoContainer;
+    private ShortenerDbContext _dbContext;
+
+    public async Task InitializeAsync()
+    {
+        _mongoContainer = new MongoDbBuilder()
+            .WithImage("docker.arvancloud.ir/mongo:latest")
+            .WithCleanUp(true)
+            .Build();
+        await _mongoContainer.StartAsync();
+    }
+
+    public ShortenerDbContext CreateDatabase()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ShortenerDbContext>();
+        var connectionString = _mongoContainer.GetConnectionString();
+        optionsBuilder.UseMongoDB(connectionString, "Test_Db");
+
+        return new ShortenerDbContext(optionsBuilder.Options);
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (_mongoContainer != null)
+        {
+            await _mongoContainer.DisposeAsync();
+        }
+
+        if (_dbContext != null)
+        {
+            await _dbContext.DisposeAsync();
+        }
+    }
+}
--- Tests/Shorten_Service_Tests.cs
+++ Tests/Shorten_Service_Tests.cs
@@ -5,7 +5,6 @@ using Shortener.Common.Models;
 using Shortener.Controllers.DTOs.Requests;
 using Shortener.Persistence;
 using Shortener.Services;
-using Testcontainers.MongoDb;
 
 namespace Tests;
 
@@ -80,42 +79,5 @@ public class ShortenServiceTests : IClassFixture<BaseFixture>
         Assert.Contains(result, u =>
             u.LongUrl == sampleUrl
             && u.ShortCode == sampleShortCode);
-    }
-}
-
-public class BaseFixture : IAsyncLifetime
-{
-    private MongoDbContainer _mongoContainer;
-    private ShortenerDbContext _dbContext;
-
-    public async Task InitializeAsync()
-    {
-        _mongoContainer = new MongoDbBuilder()
-            .WithImage("docker.arvancloud.ir/mongo:latest")
-            .WithCleanUp(true)
-            .Build();
-        await _mongoContainer.StartAsync();
-    }
-
-    public ShortenerDbContext CreateDatabase()
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<ShortenerDbContext>();
-        var connectionString = _mongoContainer.GetConnectionString();
-        optionsBuilder.UseMongoDB(connectionString, "Test_Db");
-
-        return new ShortenerDbContext(optionsBuilder.Options);
-    }
-
-    public async Task DisposeAsync()
-    {
-        if (_mongoContainer != null)
-        {
-            await _mongoContainer.DisposeAsync();
-        }
-
-        if (_dbContext != null)
-        {
-            await _dbContext.DisposeAsync();
-        }
     }
 }
