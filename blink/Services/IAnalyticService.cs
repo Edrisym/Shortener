2e1c5387d2c9a59df83119b637e6835f450d0fea
Eddie <edrismoavenghafouri@gmail.com>
2025-04-10 16:33:07 +0330

    getting the view and Analytics based on user

--- blink/Services/IAnalyticService.cs
+++ blink/Services/IAnalyticService.cs
@@ -7,18 +7,30 @@ public interface IAnalyticService
         CancellationToken cancellationToken);
 }
 
-public class AnalyticService(ShortenerDbContext db) : IAnalyticService
+public class AnalyticService(
+    ShortenerDbContext db,
+    IRedisCacheService redis) : IAnalyticService
 {
     public async Task<List<UrlAnalyticResponse>> GetUrlAnalytics(UrlAnalyticRequest request,
         CancellationToken cancellationToken)
     {
-        return await db.Urls
+        var urls = await db.Urls
+            .AsNoTracking()
             .Where(c => c.CreatedBy == request.CreatedBy)
-            .Select(x => new UrlAnalyticResponse
+            .ToListAsync(cancellationToken);
+
+        var codes = urls
+            .Select(x => x.ShortCode);
+
+        var count = await redis
+            .GetVisitCounter(codes);
+
+        return urls.Select(
+            x => new UrlAnalyticResponse
             {
                 OriginalUrl = x.LongUrl,
                 ShortCode = x.ShortCode,
-                // ViewCount = x.Viewers.Count
-            }).ToListAsync(cancellationToken);
+                VisitCount = count
+            }).ToList();
     }
 }
--- blink/Services/IRedisCacheService.cs
+++ blink/Services/IRedisCacheService.cs
@@ -12,43 +12,56 @@ public interface IRedisCacheService
     Task<T?> GetCacheValueAsync<T>(string key);
 
     Task PersistAndIncrementCounterAsync(string key);
+    Task<int> GetVisitCounter(IEnumerable<string> codes);
 }
 
 public class RedisCacheService(IConnectionMultiplexer redis) : IRedisCacheService
 {
     private const string VisitCounter = "visit_counter_{0}";
+    private readonly IDatabase _db = redis.GetDatabase();
+
 
     public async Task SetCacheValueAsync<T>(
         string key,
         T value,
         TimeSpan? expiration = null)
     {
-        var db = redis.GetDatabase();
         var json = JsonSerializer.Serialize(value);
-        await db.StringSetAsync(key, json, expiration, flags: CommandFlags.FireAndForget);
-        await db.KeyPersistAsync(string.Format(VisitCounter, key), flags: CommandFlags.FireAndForget);
+        await _db.StringSetAsync(key, json, expiration, flags: CommandFlags.FireAndForget);
+        await _db.KeyPersistAsync(string.Format(VisitCounter, key), flags: CommandFlags.FireAndForget);
     }
 
     public async Task<T?> GetCacheValueAsync<T>(
         string key)
     {
-        var db = redis.GetDatabase();
-        var json = await db.StringGetAsync(key);
+        var json = await _db.StringGetAsync(key);
         return json.HasValue ? JsonSerializer.Deserialize<T>(json!) : default;
     }
 
     public async Task PersistAndIncrementCounterAsync(
         string key)
     {
-        var db = redis.GetDatabase();
         try
         {
-            await db.StringIncrementAsync(string.Format(VisitCounter, key), flags: CommandFlags.FireAndForget);
+            await _db.StringIncrementAsync(string.Format(VisitCounter, key), flags: CommandFlags.FireAndForget);
         }
         catch (RedisException ex)
         {
             Console.WriteLine($"Error persisting and incrementing counter: {ex.Message}");
             throw;
         }
+    }
+
+    public async Task<int> GetVisitCounter(IEnumerable<string> codes)
+    {
+        var redisKeys = codes.Select(code =>
+            (Code: code, RedisKey: $"visit_counter_{code}")).ToList();
+
+        var redisTasks = redisKeys.Select(k =>
+            _db.StringGetAsync(k.RedisKey)
+                .ContinueWith(t => new { k.Code, Count = (int?)t.Result ?? 0 })).ToArray();
+        var results = await Task.WhenAll(redisTasks);
+
+        return results.Sum(r => r.Count);
     }
 }
